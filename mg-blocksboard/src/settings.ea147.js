b794d91f6964f4b8b1b8501f1fcf25b11df60a30
ovenkiller <light7777777@foxmail.com>
2021-04-04 15:31:11 +0800

    Site updated: 2021-04-04 15:31:09

---
+++ mg-blocksboard/src/settings.ea147.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/mainMenu.fire",orientation:"",jsList:[],bundleVers:{internal:"45e63",resources:"9bd52",main:"92994"}};
