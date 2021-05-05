a12fa5c5c96c960089148af0e113709266cb83a3
ovenkiller <light7777777@foxmail.com>
2021-05-05 23:21:32 +0800

    Site updated: 2021-05-05 23:21:30

--- mg-blocksboard/src/settings.affe9.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/mainMenu.fire",orientation:"",jsList:[],bundleVers:{internal:"45e63",resources:"9bd52",main:"8c293"}};
